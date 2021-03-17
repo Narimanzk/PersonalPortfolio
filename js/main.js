30e9a5c1897cae31a96473f0706b211d6c07732e
Narry <nariman.zendehrooh@gmail.com>
2021-03-17 18:49:54 -0400

    started preloader

--- js/main.js
+++ js/main.js
@@ -316,3 +316,12 @@ function bodyScrollingToggle() {
     }
   });
 })();
+
+
+window.addEventListener("load", () =>{
+  // preloader
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() =>{
+    document.querySelector(".preloader").style.display="none";
+  },600)
+})
